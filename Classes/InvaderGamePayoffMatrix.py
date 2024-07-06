be494f5471a11bc740eb57da81793f029a9b070e
Vamsi Kurakula <vamsikurakula@gmail.com>
2024-07-06 15:10:24 -0500

    Update linting

--- Classes/InvaderGamePayoffMatrix.py
+++ Classes/InvaderGamePayoffMatrix.py
@@ -18,27 +18,41 @@ class InvaderGamePayoffMatrix:
         self.S = S
         self.D = D
         self.A = A
-        self.B = D/2 #Damaged in a blocked attack 
+        self.B = D/2 #Damaged in a blocked attack
         self.Time = 0
         self.Game = {}
 
     def updateGame(self):
         """
         # Updates game given current state of variables 
-        """        
+        """
         # Invader being Passive
-        self.Game['c-c-p'] = [self.V / 2 * self.S, self.V / 2 * self.S, -1 * (self.V *self.S)]
-        self.Game['c-d-p'] = [0, self.V, -1 * (self.V)]
-        self.Game['d-c-p'] = [self.V, 0, -1 * (self.V)]
-        self.Game['d-d-p'] = [(self.V - self.C)/2, (self.V - self.C)/2, -1 *(self.V - self.C)]
+        self.Game['c-c-p'] = [self.V / 2 * self.S, # Human 1
+                              self.V / 2 * self.S, # Human 2
+                              -1 * (self.V *self.S)] # Invader
+        self.Game['c-d-p'] = [0,
+                              self.V,
+                              -1 * (self.V)]
+        self.Game['d-c-p'] = [self.V,
+                              0,
+                              -1 * (self.V)]
+        self.Game['d-d-p'] = [(self.V - self.C)/2,
+                              (self.V - self.C)/2,
+                              -1 *(self.V - self.C)]
 
         # Invader being Active
-        self.Game['c-c-a'] = [(self.V/2* self.S)-self.B, (self.V/2*self.S)-self.B, -1 * ((self.V * self.S) - (2*self.B)+ self.A) ]
-        self.Game['c-d-a'] = [-self.D, self.V - self.D, -1 * (self.V - (2* self.D)  + self.A)]
-        self.Game['d-c-a'] = [self.V - self.D, -self.D, -1 * (self.V - (2* self.D) + self.A)]
-        self.Game['d-d-a'] = [self.V/2 - self.C - self.D, self.V/2 - self.C - self.D, -1 *(self.V - (2* self.C) - (2*self.D)  + self.A)]
-
-        return
+        self.Game['c-c-a'] = [(self.V/2* self.S)-self.B,
+                              (self.V/2*self.S)-self.B,
+                              -1 * ((self.V * self.S) - (2*self.B)+ self.A) ]
+        self.Game['c-d-a'] = [-self.D,
+                              self.V - self.D,
+                              -1 * (self.V - (2* self.D)  + self.A)]
+        self.Game['d-c-a'] = [self.V - self.D,
+                              -self.D,
+                              -1 * (self.V - (2* self.D) + self.A)]
+        self.Game['d-d-a'] = [self.V/2 - self.C - self.D,
+                              self.V/2 - self.C - self.D,
+                              -1 *(self.V - (2* self.C) - (2*self.D)  + self.A)]
 
     def __repr__(self):
 

97c2978621fd4691b66256a5cb6bc3835e4bb2f1
Vamsi Kurakula <vamsikurakula@gmail.com>
2024-07-06 15:06:22 -0500

    Lint main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+'''
+Main driver file for repository
+'''
+
 from Classes.InvaderGamePayoffMatrix import InvaderGamePayoffMatrix
 
 # Human Focused Variables
-V = 10 # Resource gain 
+V = 10 # Resource gain
 C = 5 # Cost of Self Interest
 S = 1.5 # Synergy Factor
 
-# Invader Focused Variables 
+# Invader Focused Variables
 A = 2 # Cost of Attacking from Invader
 D = 20 # Damage from Invader
 

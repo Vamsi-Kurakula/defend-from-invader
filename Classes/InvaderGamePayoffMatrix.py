778c19c04979032789a8a524be710bd9a0413890
Vamsi Kurakula <vamsikurakula@gmail.com>
2024-07-11 19:31:34 -0500

    Update d with s

--- Classes/InvaderGamePayoffMatrix.py
+++ Classes/InvaderGamePayoffMatrix.py
@@ -30,13 +30,13 @@ class InvaderGamePayoffMatrix:
         self.Game['c-c-p'] = [self.V / 2 * self.S, # Human 1
                               self.V / 2 * self.S, # Human 2
                               -1 * (self.V *self.S)] # Invader
-        self.Game['c-d-p'] = [0,
+        self.Game['c-s-p'] = [0,
                               self.V,
                               -1 * (self.V)]
-        self.Game['d-c-p'] = [self.V,
+        self.Game['s-c-p'] = [self.V,
                               0,
                               -1 * (self.V)]
-        self.Game['d-d-p'] = [(self.V - self.C)/2,
+        self.Game['s-s-p'] = [(self.V - self.C)/2,
                               (self.V - self.C)/2,
                               -1 *(self.V - self.C)]
 
@@ -44,13 +44,13 @@ class InvaderGamePayoffMatrix:
         self.Game['c-c-a'] = [(self.V/2* self.S)-self.B,
                               (self.V/2*self.S)-self.B,
                               -1 * ((self.V * self.S) - (2*self.B)+ self.A) ]
-        self.Game['c-d-a'] = [-self.D,
+        self.Game['c-s-a'] = [-self.D,
                               self.V - self.D,
                               -1 * (self.V - (2* self.D)  + self.A)]
-        self.Game['d-c-a'] = [self.V - self.D,
+        self.Game['s-c-a'] = [self.V - self.D,
                               -self.D,
                               -1 * (self.V - (2* self.D) + self.A)]
-        self.Game['d-d-a'] = [self.V/2 - self.C - self.D,
+        self.Game['s-s-a'] = [self.V/2 - self.C - self.D,
                               self.V/2 - self.C - self.D,
                               -1 *(self.V - (2* self.C) - (2*self.D)  + self.A)]
 

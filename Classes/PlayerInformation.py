08d3b39b43ff4ecd32f9ceeb649f7edf0ec98531
Vamsi Kurakula <vamsikurakula@gmail.com>
2024-07-13 12:40:38 -0500

    Update game to have basic strategy mechanics

--- Classes/PlayerInformation.py
+++ Classes/PlayerInformation.py
@@ -6,10 +6,28 @@ class PlayerInformation():
         self.name = name
         self.action_set = action_set
         self.strategy = strategy
+        self.total_payoff = 0
 
     def get_action(self):
         '''
         Given an action set and strategy, get a action
         '''
 
-        return random.choices(population=self.action_set, weights=self.strategy)[0]
+        return random.choices(population=self.action_set, weights=[self.strategy, 1-self.strategy])[0]
+
+    def update_payoff(self, my_payoff):
+        '''
+        Updates total payoff gained during game
+        '''
+        self.total_payoff += my_payoff
+
+    def update_strategy(self, my_payoff, game_payoff):
+        
+        if my_payoff >=0:
+            self.strategy += .1
+        else: 
+            self.strategy -= .1
+
+        # Make sure we bound the strategy between 0 and 1
+        self.strategy = min([1, self.strategy])
+        self.strategy = max([0, self.strategy])
--- solo-game.py
+++ solo-game.py
@@ -17,19 +17,41 @@ DAMAGE = 20 # Damage from Invader
 ATTACK = 2 # Cost of Attacking from Invader
 
 # Simulation settings  
-ROUNDS = 20
-human_1 = PlayerInformation(name='Human_1', action_set=['c', 's'], strategy=[50,50])
-human_2 = PlayerInformation(name='Human_2', action_set=['c', 's'], strategy=[50,50])
-invader = PlayerInformation(name='Invader', action_set=['p', 'a'], strategy=[50,50])
+ROUNDS = 100
+human_1 = PlayerInformation(name='Human_1', action_set=['c', 's'], strategy=.5)
+human_2 = PlayerInformation(name='Human_2', action_set=['c', 's'], strategy=.5)
+invader = PlayerInformation(name='Invader', action_set=['p', 'a'], strategy=.5)
 
 # Create and initalize game
 game = InvaderGamePayoffMatrix(VALUE, COST, SYNERGY, DAMAGE, ATTACK)
-game.update_game()
 
 for i_round in range(ROUNDS):
-    
-    print(game.get_payoffs(human_1.get_action(),
-                           human_2.get_action(),
-                           invader.get_action()))
+    game.update_game()
 
-#game.print_gamestate()
+    payoffs = game.get_payoffs(human_1.get_action(),
+                           human_2.get_action(),
+                           invader.get_action())
+    
+    human_1.update_payoff(payoffs[0])
+    human_2.update_payoff(payoffs[1])
+    invader.update_payoff(payoffs[2])
+
+    human_1.update_strategy(payoffs[0], payoffs)
+    human_2.update_strategy(payoffs[1], payoffs)
+    invader.update_strategy(payoffs[2], payoffs)
+
+
+game.print_gamestate()
+
+output_string = f'''Total Payoffs:
+Human 1: {human_1.total_payoff}
+Human 2: {human_2.total_payoff}
+Invader: {invader.total_payoff}
+
+Strategy Sets:
+Human 1: [c: {round(human_1.strategy, 2)}, s: {round(1 - human_1.strategy,2 )}]
+Human 2: [c: {round(human_2.strategy, 2)}, s: {round(1 - human_2.strategy,2 )}]
+Invader: [p: {round(invader.strategy, 2)}, a: {round(1 - invader.strategy,2 )}]
+'''
+
+print(output_string)
